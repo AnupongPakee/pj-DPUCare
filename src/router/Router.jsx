239c8db7008dc17500cd85af2be16de86e8b012a
AnupongPakee <anuphong4407@gmail.com>
2025-01-26 15:07:04 +0700

    update routers

---
+++ src/router/Router.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import APP from "../App"
+import REGISTER from "../components/Register"
+import LOGIN from "../components/Login"
+import SETTING from "../components/Setting"
+import CHAT from "../components/Chat"
+import ADMIN from "../admin/Admin"
+import TEST from "../test/Test"
+
+const router = createBrowserRouter([
+    {
+        path: "/pj-DPUCare/",
+        element: <APP/>
+    },
+    {
+        path: "/pj-DPUCare/register",
+        element: <REGISTER/>
+    },
+    {
+        path: "/pj-DPUCare/login",
+        element: <LOGIN/>
+    },
+    {
+        path: "/pj-DPUCare/setting",
+        element: <SETTING/>
+    },
+    {
+        path: "/pj-DPUCare/chat",
+        element: <CHAT/>
+    },
+    {
+        path: "/pj-DPUCare/admin",
+        element: <ADMIN/>
+    },
+    {
+        path: "/pj-DPUCare/test",
+        element: <TEST/>
+    }
+])
+
+export default router;
