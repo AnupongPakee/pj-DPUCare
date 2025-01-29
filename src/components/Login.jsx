2fcf2b5262a5ace0b406a281413ef896f3611811
AnupongPakee <anuphong4407@gmail.com>
2025-01-29 15:22:39 +0700

    update ux/ui Login.jsx + Responsive v0.0

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import STYLE from "../style/Style"
 
 function Login() {
+  const [form, setForm] = useState({})
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const showPass = () => {
+    let checkbox = document.getElementById("check_password")
+    const password = document.getElementById("password")
+    if (checkbox.checked == true) {
+      password.setAttribute("type", "text");
+    } else {
+      password.setAttribute("type", "password");
+    }
+  }
   return (
-    <div>Login</div>
+    <div className='container login' style={STYLE.container}>
+      <div className="content" id='content-lgn'>
+        <div className="left"></div>
+        <div className="right">
+          <form action="" id='form'>
+            <h1 style={STYLE.font_family.th}>เข้าสู่ระบบ</h1>
+            <input type="text" name='username' style={STYLE.font_family.th} placeholder='ชื่อผู้ใช้ / อีเมล์' onChange={(e) => handleChange(e)} />
+            <input type="password" name='password' id='password' style={STYLE.font_family.th} placeholder='รหัสผ่าน' />
+            <div className="box-check">
+              <input type="checkbox" name="check_password" id="check_password" onClick={showPass} />
+            </div>
+            <button type="submit">ลงชื่อเข้าใช้</button>
+          </form>
+        </div>
+      </div>
+      <div className="not-support">
+        <h1 style={STYLE.font_family.en}>Not Support</h1>
+      </div>
+    </div>
   )
 }
 
