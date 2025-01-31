16f76987fbb6ff2f0bd42fa14c2d21ae8daf7641
AnupongPakee <anuphong4407@gmail.com>
2025-01-31 14:37:49 +0700

    update ux/ui App

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="content-1">
         <div className="left">
           <h1 style={STYLE.font_family.th}>แชทบอทให้คำปรึกษา</h1>
-          <p style={STYLE.font_family.th}> DPU Care เป็นบริการแชทบอทที่ให้คำปรึกษาด้านความเครียด โดยมีการนำเอาแนวคิดของ RAG เข้ามาช่วยในการสืบค้นข้อมูล และยังนำเอา CoT เข้ามาช่วยในการวิเคราะห์และประเมินอาการ</p>
+          <p style={STYLE.font_family.th}> DPU Care เป็นบริการแชทบอทที่ให้คำปรึกษาด้านความเครียด</p>
           <div className="btn">
             <button style={STYLE.font_family.th} onClick={() => navigate("/pj-DPUCare/login")}>เข้าสู่ระบบ</button>
             <button style={STYLE.font_family.th} onClick={() => navigate("/pj-DPUCare/register")}>สร้างบัญชี</button>
@@ -18,29 +18,6 @@ function App() {
           <div className="blur"></div>
         </div>
         <div className="right"></div>
-      </div>
-      <div className="content-2">
-        <h1 style={STYLE.font_family.th}>บริการหลัก</h1>
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <h3 style={STYLE.font_family.th}>คำปรึกษา</h3>
-              </td>
-              <td>
-                <p style={STYLE.font_family.th}>เรามีการใช้แนวคิด RAG ( Retrieval-Augmented Generation ) ที่ช่วยในการสืบค้นข้อมูลที่เชื่อถือได้ ทำให้คุณมั่นใจในข้อมูลที่ได้รับ เรามุ่งเน้นให้บริการที่มีประสิทธิภาพและตอบสนองต่อความต้องการของผู้ใช้</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h3 style={STYLE.font_family.th}>วิเคราะห์</h3>
-              </td>
-              <td>
-                <p style={STYLE.font_family.th}>เรามีการใช้แนวคิด CoT ( Chain Of Thought ) มาใช้รวมกับ LLM ที่จะช่วยในการวิเคราะห์ข้อมูลเพื่อให้คุณได้ข้อมูลที่ถูกต้อง และประเมินอาการเพื่อให้คำปรึกษาที่เหมาะสม</p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
       <div className="not-support">
         <h1 style={STYLE.font_family.en}>Not Support</h1>
